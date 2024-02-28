b081f93d4ae596e62fc985fe4d9c12edc0a3f23b
sonwanejay <156161917+sonwanejay@users.noreply.github.com>
2024-02-28 09:32:38 +0000

    whileloop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+let mArray = ["Flah", "Batman", "Superman"]
+
+// let arr = 0
+// while (arr <mArray.length) {
+//     // console.log(`value is ${mArray[arr]}`);
+//     arr = arr + 1
+    
+// }
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
